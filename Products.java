23f2376b66ebc72a750144bf640130bccb05f384
Arianne Ghislaine Rull <arianneghislaine@gmail.com>
2020-07-02 19:13:32 -0400

    Update Products.java

--- Products.java
+++ Products.java
@@ -1,12 +1,3 @@
-/* 
-It looks like the order for that class is a bit reversed
-
-You should be initializing the firstProduct and secondProduct at the 
-start of that class and from that initializing the variables within it afterwards
-
-At its current state, the two Products have no initialized variables other than its name
-*/
-
 public class Products {
     private String productName;
     private double price;
